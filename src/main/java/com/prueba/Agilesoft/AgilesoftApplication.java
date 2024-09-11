4ef19414e794178b1e58cbca160423b6d199d96f
andersonur <andersonur@hotmail.com>
2024-09-11 18:45:12 -0500

    Configuracion inicial

---
+++ src/main/java/com/prueba/Agilesoft/AgilesoftApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.Agilesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilesoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilesoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/Agilesoft/AgilesoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.Agilesoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilesoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
